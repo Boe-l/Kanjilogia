0cd7811e5830af382cbac75ba4ab42b3965f72a5
Boem <levifreire100@gmail.com>
2025-01-12 08:38:35 -0300

    fixed history bug

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3088920994"
+    serviceWorkerVersion: "25471081"
   }
 });
