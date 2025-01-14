7ec10844c486fd976ddfe297be0f0e223d6a3c37
Boem <levifreire100@gmail.com>
2025-01-14 05:13:11 -0300

    fix main and game_screen

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "747810809"
+    serviceWorkerVersion: "2372888286"
   }
 });
